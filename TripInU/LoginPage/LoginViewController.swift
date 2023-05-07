bee666df369f2dc15bb89dd224b2883ed6ca4e18
dami <dami8789@gmail.com>
2023-05-07 15:01:32 +0900

    feat:Firebase연동및 로그인연결

--- TripInU/LoginPage/LoginViewController.swift
+++ TripInU/LoginPage/LoginViewController.swift
@@ -5,25 +5,41 @@
 //  Created by 박다미 on 2023/05/05.
 //
 
+
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty
+        else {
+            // 이메일과 비밀번호 필드가 모두 채워졌는지 확인
+            emailLabel.text = "모든 필드를 채워주세요"
+            return
+        }
+        
+        // 이메일과 비밀번호로 로그인
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+            if let error = error {
+                // 로그인 실패
+                self?.emailLabel.text = "로그인 실패: \(error.localizedDescription)"
+            } else {
+                // 로그인 성공
+                guard let user = result?.user else { return }
+                self?.emailLabel.text = "로그인 성공! 사용자 이메일: \(user.email ?? "")"
+            }
+        }
     }
-    */
-
 }
